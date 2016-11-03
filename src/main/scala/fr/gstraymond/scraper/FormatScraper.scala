2de57db141d32eb1abf07c716c253aceb3b1151c
Guillaume Saint-Raymond <tcherno@gmail.com>
2016-11-03 21:21:50 +0100

    Format fix

--- src/main/scala/fr/gstraymond/scraper/FormatScraper.scala
+++ src/main/scala/fr/gstraymond/scraper/FormatScraper.scala
@@ -2,11 +2,12 @@ package fr.gstraymond.scraper
 
 import fr.gstraymond.model.ScrapedFormat
 import fr.gstraymond.scraper.format._
+import fr.gstraymond.utils.Log
 
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.Future
 
-object FormatScraper extends MTGSalvationScraper {
+object FormatScraper extends MTGSalvationScraper with Log{
 
   val scrapers = Seq(
     StandardFormatScrap,
@@ -21,12 +22,14 @@ object FormatScraper extends MTGSalvationScraper {
     Future.sequence {
       scrapers.map { scraper =>
         scrap(scraper.path, followRedirect = true).map { doc =>
-          ScrapedFormat(
+          val format = ScrapedFormat(
             scraper.name,
             scraper.currentRotation(doc),
             scraper.bannedCards(doc),
             scraper.restrictedCards(doc)
           )
+          log.info(s"format scraped: $format")
+          format
         }
       }
     }
