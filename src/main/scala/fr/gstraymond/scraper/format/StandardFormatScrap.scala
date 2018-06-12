50e59ea09b9528b9c320f29484c646bd9a5626e8
Guillaume Saint-Raymond <tcherno@gmail.com>
2018-06-12 21:10:08 +0200

    Fix scrap standard

--- src/main/scala/fr/gstraymond/scraper/format/StandardFormatScrap.scala
+++ src/main/scala/fr/gstraymond/scraper/format/StandardFormatScrap.scala
@@ -7,7 +7,7 @@ object StandardFormatScrap extends FormatScrap {
   override val path = "/Standard"
 
   override def bannedCards(doc: Document): Seq[String] =
-    doc.getTexts("#mw-content-text > ul:nth-child(16) > li > a")
+    doc.getTexts("#mw-content-text > ul:nth-child(17) > li > a")
 
   override def currentRotation(doc: Document): Seq[String] =
     doc.getTexts(".wikitable > tbody:nth-child(1) > tr > td > i > a").map(_.split(" \\(").head)
