48767e4791212b1c57f382b3ef102343ee8519ec
Guillaume Saint-Raymond <tcherno@gmail.com>
2018-11-17 13:49:56 +0100

    Fix scap format

--- src/main/scala/fr/gstraymond/scraper/format/CommanderFormatScrap.scala
+++ src/main/scala/fr/gstraymond/scraper/format/CommanderFormatScrap.scala
@@ -7,7 +7,7 @@ object CommanderFormatScrap extends FormatScrap {
   override val path = "/Vintage"
 
   override def bannedCards(doc: Document): Seq[String] =
-    doc.getTexts("#mw-content-text > ul:nth-child(8) > li > a").drop(3) ++
+    doc.getTexts(".mw-parser-output:nth-child(1) > ul:nth-child(8) > li > a").drop(3) ++
       // Any card referencing ante
       // Any card with Conspiracy card type
       Seq(
--- src/main/scala/fr/gstraymond/scraper/format/StandardFormatScrap.scala
+++ src/main/scala/fr/gstraymond/scraper/format/StandardFormatScrap.scala
@@ -7,7 +7,7 @@ object StandardFormatScrap extends FormatScrap {
   override val path = "/Standard"
 
   override def bannedCards(doc: Document): Seq[String] =
-    doc.getTexts("#mw-content-text > ul:nth-child(17) > li > a")
+    doc.getTexts(".mw-parser-output:nth-child(1) > ul:nth-child(17) > li > a")
 
   override def currentRotation(doc: Document): Seq[String] =
     doc.getTexts(".wikitable > tbody:nth-child(1) > tr > td > i > a").map(_.split(" \\(").head)
