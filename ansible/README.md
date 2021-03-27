125a86f7ed06960cd0172580001ef840c31d7c49
Guillaume Saint-Raymond <tcherno@gmail.com>
2021-03-27 14:30:09 +0100

    Updated ansible scrits for VPS migration
    - based on github release to download last version

--- ansible/README.md
+++ ansible/README.md
@@ -10,16 +10,23 @@ vagrant up
  - `.ssh/id_rsa.pub`
  - `.htpasswd`
 
+# Local install
+
+Enable `network.host: 0.0.0.0` in elasticsearch.yml
+Disable iptable
+
 # Fix to apply
 https://github.com/nickjj/ansible-iptables/pull/2/files
+
+# DNS
+
+Set DNS search.mtg-search.com and report.mtg-search.com to the VPS, see steps in playbook.yml
 
 # Provision
 ```bash
 ansible-playbook playbook.yml -i hosts.ini
 ```
 
-# Certbox
+# Release
 
-```bash
-sudo certbox --nginx
-```
+Bump version, `./gradlew distZip` and create release on github 
--- ansible/templates/doZeMagic.sh
+++ ansible/templates/doZeMagic.sh
@@ -1,27 +1,25 @@
 #!/bin/bash
 set -x
 today=`date +%F`
+app_dir=app
 
-rm /tmp/import_${today}.html
-cd /root/git/mtg-index-scala
+cd /root/git/pics
 git pull
-git submodule update --init --remote
-./gradlew --no-daemon clean installDist
-JAVA_OPTS="-Xmx512m" build/install/mtg-index-scala/bin/mtg-index-scala >> /tmp/import_${today}.html
-./gradlew --no-daemon clean
-rm -rf src/main/resources/output/
-rm -rf src/main/resources/scrap/
+cd ..
+mkdir -p $app_dir
+rm -rf $app_dir/*
+cd $app_dir
+curl -sL https://api.github.com/repos/gstraymond/mtg-index-scala/releases/latest | \
+  jq -r '.assets[].browser_download_url' | \
+  xargs -I {} curl -L {} -so release.zip
+unzip release.zip
+cd ..
+JAVA_OPTS=-Xmx512m find . -name 'mtg-index-scala' -exec {} \; > /tmp/import_${today}.html
+rm -rf /tmp/mtg-search/
 cd pics
 git add .
 git commit -m "Import ${today}"
 git push
-cd ..
-git add .
-git commit -m "Import ${today}"
-git push
-echo -e "Subject: MGS import ${today}\n" > /tmp/mail.txt
-cat /tmp/import_${today}.html >> /tmp/mail.txt
-cat /tmp/mail.txt | msmtp tcherno@gmail.com
 sed -i '1i <pre>' /tmp/import_${today}.html
 echo "</pre>" >> /tmp/import_${today}.html
 cp /tmp/import_${today}.html /www/report/imports/.
